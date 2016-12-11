2efc4dacc680125bccb7c34118b4b901b4d08d61
Adrien Navratil <adrien1975@live.fr>
2016-12-11 12:54:02 +0100

    Vector 3f

---
+++ library/util/src/main/java/com/ylinor/library/util/math/Vector3f.java
@@ -0,0 +1,24 @@
+package com.ylinor.library.util.math;
+
+/**
+ * Un vecteur 3D (floats)
+ *
+ * @author Litarvan
+ */
+public class Vector3f
+{
+    public float x;
+    public float y;
+    public float z;
+
+    public Vector3f()
+    {
+    }
+
+    public Vector3f(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+}
