d7c34d2578b75f463e30396591a1868277d8a916
victor <wytrem@gmail.com>
2017-02-17 19:18:35 +0100

    osef

--- client-core/src/main/java/com/ylinor/client/ui/YlinorBigTextButton.java
+++ client-core/src/main/java/com/ylinor/client/ui/YlinorBigTextButton.java
@@ -3,7 +3,6 @@ package com.ylinor.client.ui;
 import com.kotcrab.vis.ui.widget.VisTextButton;
 
 public class YlinorBigTextButton extends VisTextButton {
-	
 	public YlinorBigTextButton(String text) {
 		super(text);
 	}
