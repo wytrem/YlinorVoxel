8b9e09d1f0e02085eeac9a099f997b330476ab34
victor <wytrem@gmail.com>
2017-04-21 16:39:39 +0200

    Fix mac

--- bootstrap/src/main/java/com/ylinor/bootstrap/YlinorBootstrap.java
+++ bootstrap/src/main/java/com/ylinor/bootstrap/YlinorBootstrap.java
@@ -59,6 +59,11 @@ public class YlinorBootstrap {
         constructor.add(Explorer.dir(FOLDER).get(LAUNCHER_FILE));
 
         ExternalLaunchProfile profile = new ExternalLaunchProfile(MAIN_CLASS, constructor.make());
+        
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            profile.getVmArgs().add("-XstartOnFirstThread");
+        }
+        
         profile.setRedirectErrorStream(true);
         ExternalLauncher launcher = new ExternalLauncher(profile);
 
